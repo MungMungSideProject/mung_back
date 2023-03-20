2727f97e228b44f509f82f073042c0354b0baaf2
choi jaemin <foertonw5@naver.com>
2023-03-21 08:04:25 +0900

    init : [최재민]프로젝트 서버 초기 환경 설정

---
+++ springServer/src/main/java/com/example/mungmung/MungMungApplication.java
@@ -0,0 +1,13 @@
+package com.example.mungmung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MungMungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MungMungApplication.class, args);
+	}
+
+}
